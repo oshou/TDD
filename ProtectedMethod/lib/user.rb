01e7fc28d1c75f34ffc37be9844b0ca7053aa19f
oshou <adf1985adf@gmail.com>
2020-01-03 00:06:56 +0900

    post

--- ProtectedMethod/lib/user.rb
+++ ProtectedMethod/lib/user.rb
@@ -11,6 +11,7 @@ class User
   end
 
   protected
+
   def weight
     @weight
   end
--- ProtectedMethod/test/user_test.rb
+++ ProtectedMethod/test/user_test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/reporters"
 require "../lib/user"
 
 class UserTest < Minitest::Test
@@ -13,5 +14,13 @@ class UserTest < Minitest::Test
 
   def test_bar_heavier_than_foo?
     assert_equal @bar.heavier_than?(@foo), true
+  end
+
+  def test_foo_weight
+    assert_equal @foo.send(:weight), 50
+  end
+
+  def test_bar_weight
+    assert_equal @bar.send(:weight), 60
   end
 end
