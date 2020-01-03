da447aa94b56370dac8b3c4513efd4d3f4d90a22
oshou <adf1985adf@gmail.com>
2020-01-03 09:27:50 +0900

    post

--- env_monolog/env_monolog.php
+++ env_monolog/env_monolog.php
@@ -2,10 +2,16 @@
 
 require "vendor/autoload.php";
 
-use Carbon\Carbon;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+echo getenv('S3_BUCKET');
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$log = new Logger('composer');
-$log->pushHandler(new StreamHandler(__DIR__ . '/your.log', Logger::WARNING));
-$log->warning(Carbon::now());
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler(__DIR__ . '/your-aaa.log', Logger::WARNING));
+$log->warning('Foo');
+$log->error('Bar');
